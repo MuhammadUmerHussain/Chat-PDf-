f74d1970c9f7acf264fa4ad661fa637251eb73de
MuhammadUmerHussain <umerhussain231123@gmail.com>
2024-01-16 04:13:42 +0500

    env and aws set also file uploading

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,5 +1,6 @@
 
 "use client"; //in next when clickable event or use state then use client for client side rendering 
+import { uploadToS3 } from '@/lib/s3';
 import { Inbox } from 'lucide-react';
 import React from 'react'
 import {useDropzone} from "react-dropzone"
@@ -8,8 +9,23 @@ const FileUpload = () => {
         {
             accept:{"application/pdf":[".pdf"]},
             maxFiles:1,
-            onDrop:(acceptedFiles)=>{
+            onDrop: async(acceptedFiles)=>{
                 console.log(acceptedFiles)
+                const file =acceptedFiles[0]
+                if(file.size>10 *1024*1024)
+                {
+                  //bigger than 10 MB
+
+                  alert("Please upload a smaller file")
+                  return
+                }
+                try {
+                  const data =await uploadToS3(file)
+                  console.log("FileUpload "+data)
+                } catch (error) {
+                  console.log(error)
+                }
+               
             }
         }
     );
